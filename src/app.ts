021b56b7da80d8449926694ac8fbaadb12cd1fd1
Utkarsh <utkjaiswal58@gmail.com>
2024-12-23 20:14:13 +0530

    mongodb setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import express, { Express } from 'express';
-import dotenv from 'dotenv';
-
 
 const app: Express = express();
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import app from './app';
+import connectDB from './config/mongodb/database';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
